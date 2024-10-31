7870fe316cf91b2ca58c7b3d78e7335166f8ef50
anas falaq <anasfalaq94@gmail.com>
2024-10-31 15:53:34 +0100

    creation de structure et un table globale et un variable pour compter nbr de taches

--- sas.c
+++ sas.c
@@ -2,3 +2,13 @@
 #include <string.h>
 
 #define MAX_TACHES 100
+
+typedef struct {
+    char titre[50];
+    char description[200];
+    char date_echeance[11]; 
+    char priorite[5]; 
+} Tache;
+
+Tache liste_taches[MAX_TACHES];
+int nombre_taches = 0;
